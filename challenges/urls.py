d6d3682c197cadf7487fc0197fd4731a04244840
virtualgain008 <virtualgaintechnologies008@gmail.com>
2025-05-23 17:34:53 +0530

    :blossom:  define functions fot get monthwise challenges

--- challenges/urls.py
+++ challenges/urls.py
@@ -2,5 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("january", views.index),
+    path("january", views.january),
+    path("february", views.february),
+    path("march", views.march),
+    path("month/<id>", views.monthly_challenges), #pass dynamic url to the view
+
+
 ]
--- challenges/views.py
+++ challenges/views.py
@@ -1,7 +1,34 @@
-from django.http import HttpResponse
+from django.http import HttpResponse , HttpResponseNotFound
 from django.shortcuts import render
 
 # Create your views here.
 
-def index(request):
+def january(request):
     return HttpResponse("This works")
+
+def february(request):
+    return HttpResponse("Work for at least 20 minutes every day")
+
+def march(request):
+    return HttpResponse("Learn D jango for 30 minutes every day!")
+
+def monthly_challenges(request, id):
+    challenge_text = None
+    if id == "1":
+        challenge_text = "This works"
+    elif id == "2":
+        challenge_text = "Work for at least 20 minutes every day"
+    elif id == "3":
+        challenge_text = "Learn D jango for 30 minutes every day!"
+    else:
+        return HttpResponseNotFound("this month is not supported")
+    return HttpResponse(challenge_text)
+
+ 
+  
+
+     
+ 
+
+
+
